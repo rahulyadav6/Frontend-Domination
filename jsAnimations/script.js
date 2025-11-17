267b2d3c64a26349cc1ef244a11b883b7814c2b4
Rahul Kumar Yadav <ry573870@gmail.com>
2025-11-18 02:33:36 +0530

    question 7 completed

--- jsAnimations/script.js
+++ jsAnimations/script.js
@@ -33,29 +33,41 @@
 
 
 // Question 5
-let time = document.querySelector("h3");
-let startBtn = document.querySelector("#start");
-let stopBtn = document.querySelector("#stop");
-let resetBtn = document.querySelector("#reset");
-let timer = 0;
-time.innerText = timer;
-let intervalId = null;
-startBtn.addEventListener("click", (e)=>{
-    if(intervalId === null){
-        intervalId = setInterval(() => {
-            time.innerText = timer;
-            timer++;
-        }, 1000);  
-    }
-})
-stopBtn.addEventListener("click", (e)=>{
-    clearInterval(intervalId);
-    intervalId = null;
-})
+// let time = document.querySelector("h3");
+// let startBtn = document.querySelector("#start");
+// let stopBtn = document.querySelector("#stop");
+// let resetBtn = document.querySelector("#reset");
+// let timer = 0;
+// time.innerText = timer;
+// let intervalId = null;
+// startBtn.addEventListener("click", (e)=>{
+//     if(intervalId === null){
+//         intervalId = setInterval(() => {
+//             time.innerText = timer;
+//             timer++;
+//         }, 1000);  
+//     }
+// })
+// stopBtn.addEventListener("click", (e)=>{
+//     clearInterval(intervalId);
+//     intervalId = null;
+// })
 
-resetBtn.addEventListener("click", (e)=>{
-    timer = 0;
-    time.innerText = timer;
-    intervalId = null;
-})
+// resetBtn.addEventListener("click", (e)=>{
+//     timer = 0;
+//     time.innerText = timer;
+//     intervalId = null;
+// })
+
+// Question 7
+let progress = document.querySelector("#progress")
+let count = 0;
+let intervalId = setInterval(() => {
+    if(count === 100){
+        clearInterval(intervalId)
+    }
+    count++;
+    progress.style.width = count+"%"
+}, 50);
+
 
